c0f3eb2d9d868d204178ec503c7c90fc7af45420
AmeerahDA <89031541+AmeerahDA@users.noreply.github.com>
2022-12-28 00:26:24 +0300

    First Commit

---
+++ Boxing-Unboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<object> objList = new List<object>();
+        objList.Add(7);
+        objList.Add(28);
+        objList.Add(-1);
+        objList.Add(true);
+        objList.Add("chair");
+        int x =0;
+        int cast;
+        foreach (object obj in objList)
+        {
+            Console.Write(" " + obj);
+            if (obj is int)
+            {
+                cast=Convert.ToInt32(obj);
+                x=x+cast;
+            }
+        }
+        Console.WriteLine("\nThe sum of integer values is :"+x);
+
+
+
+    }
+}
